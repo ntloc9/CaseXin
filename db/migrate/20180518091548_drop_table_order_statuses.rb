758fa8f0865b0d8a6ac69b90ff5e1bfe7cfbc737
ntloc9 <ntloc9@gmail.com>
2018-05-22 21:33:50 +0700

    delete migrate 3 4

--- db/migrate/20180518091548_drop_table_order_statuses.rb
+++
@@ -1,5 +0,0 @@
-class DropTableOrderStatuses < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :order_statuses
-  end
-end
--- db/migrate/20180518091607_drop_table_orders.rb
+++
@@ -1,5 +0,0 @@
-class DropTableOrders < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :orders
-  end
-end
