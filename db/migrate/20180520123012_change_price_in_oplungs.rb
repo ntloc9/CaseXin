e3c41e4ef507c9db6bc710db7d2bb2a0d429a427
ntloc9 <ntloc9@gmail.com>
2018-05-22 21:40:13 +0700

    change migration 1

--- db/migrate/20180520123012_change_price_in_oplungs.rb
+++ db/migrate/20180520123012_change_price_in_oplungs.rb
@@ -1,6 +1,5 @@
 class ChangePriceInOplungs < ActiveRecord::Migration[5.1]
   def change
-    change_column :oplungs, :price , :decimal
     change_column :oplungs, :title, :string, null: false
   end
 end
